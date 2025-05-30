dd589dbd18c0889654b7626c0bd07c4f4050a6d3
Muhammad Osama <mohamedelshafei977@gmail.com>
2025-05-30 14:08:07 +0300

    feat(networking): add a NAT router for GKE cluster

--- network/main.tf
+++ network/main.tf
@@ -44,19 +44,19 @@ resource "google_compute_router_nat" "nat" {
 }
 
 
-# NAT ROUTER for gke subnet
-
-
-
-
 # NAT ROUTER for gke
 
-# resource "google_compute_router" "nat-router-gke" {
-#   name    = "${var.vpc_name}-gke-router"
-#   region  = google_compute_subnetwork.gke_subnet.region
-#   network = google_compute_network.vpc.id
-# }
+resource "google_compute_router" "nat-router-gke" {
+  name    = "${var.vpc_name}-gke-router"
+  region  = google_compute_subnetwork.gke_subnet.region
+  network = google_compute_network.vpc.id
+}
 
+resource "google_compute_router_nat" "nat-gke" {
+  name   = "${var.vpc_name}-nat"
+  router = google_compute_router.nat-router-gke.name
+  region = google_compute_router.nat-router-gke.region
 
-
-
+  nat_ip_allocate_option = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
